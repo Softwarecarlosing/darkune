c9ce240466f6d5b7a7def9a0664e6bffe253efd7
softwarecarlosing <infocarloscarmona@gmail.com>
2020-01-21 16:22:39 -0600

    INICIO

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,25 @@ import 'package:flutter/material.dart';
 
 
 // metodo central donde se corre/ejecuta la aplicacion
-void main() {
-  
-  // correr aplicacion 
-  runApp(new
+
+void main() => runApp(Aplication());
+
+
+class Aplication extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context){
+     return MaterialApp(title: 'Navigator',
+     home: Home(),
+     theme: ThemeData(
+       primaryColor: Color(0xFF2F0083),
+       accentColor: Color(0xFFFDD303),
+     )
+
+     );
+
+  }
 
 }
+
+
 
