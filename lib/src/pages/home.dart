a52805bfe597650572cc329e6188aaf9e098c3eb
softwarecarlosing <infocarloscarmona@gmail.com>
2020-01-21 17:24:22 -0600

    inicio de menu

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -8,11 +8,19 @@ class Home extends StatelessWidget {
  @override
  Widget build(BuildContext context){
     return Scaffold(
-    appBar: AppBar(title: Text('Navigation Drawer'),
+    appBar: AppBar(title: Text('I F P'),
     ),
     drawer: Drawer(
       child: ListView(
        children: <Widget>[
+         UserAccountsDrawerHeader(
+           accountName: Text(''),
+           accountEmail: Text('Bienvenido al menu'),
+           currentAccountPicture: CircleAvatar(
+             backgroundColor: Colors.blue,
+             child: Text('Ifp', style: TextStyle(fontSize: 40.0))
+           ),
+         ),
          ListTile(
            title: Text('Contacto'),
            onTap: (){
